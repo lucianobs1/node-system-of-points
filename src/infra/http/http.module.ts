e274b8621199411b6be5f7567750f4fc6c894981
lucianobs1 <lucianotetsu@gmail.com>
2024-01-24 22:43:58 -0300

    refactor: create http module competitor

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,37 +1,10 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from '../database/database.module';
-import { MulterModule } from '@nestjs/platform-express';
-import { MulterConfig } from 'src/shared/multer.config';
-import { MulterStorageProvider } from 'src/shared/multer-storage-provider';
-import { IStorageProvider } from 'src/shared/storage-provider';
-import { CompetitorAvatarController } from './controllers/competitors.avatar.controller';
-import { CompetitorsController } from './controllers/competitors.controller';
-import { UpdateAvatarCompetitorUseCase } from 'src/app/use-cases/update-avatar-competitor-use-case';
-import { UpdateCompetitorUseCase } from 'src/app/use-cases/update-competitor-use-case';
-import { CreateCompetitorUseCase } from 'src/app/use-cases/create-competitor-use-case';
-import { GetCompetitorsUseCase } from 'src/app/use-cases/get-competitors-use-case';
-import { GetCompetitorInfosUseCase } from 'src/app/use-cases/get-competitor-infos';
-import { DeleteCompetitorUseCase } from 'src/app/use-cases/delete-competitor-use-case';
+import { HttpCompetitor } from './http.competitor';
 
 @Module({
-  imports: [
-    DatabaseModule,
-    MulterModule.registerAsync({
-      useClass: MulterConfig,
-    }),
-  ],
-  controllers: [CompetitorsController, CompetitorAvatarController],
-  providers: [
-    CreateCompetitorUseCase,
-    UpdateCompetitorUseCase,
-    UpdateAvatarCompetitorUseCase,
-    GetCompetitorsUseCase,
-    GetCompetitorInfosUseCase,
-    DeleteCompetitorUseCase,
-    {
-      provide: IStorageProvider,
-      useClass: MulterStorageProvider,
-    },
-  ],
+  imports: [DatabaseModule, HttpCompetitor],
+  controllers: [],
+  providers: [],
 })
 export class HttpModule {}
